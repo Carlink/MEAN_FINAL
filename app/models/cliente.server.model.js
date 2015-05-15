765c5d71bdfdb09415206df42ea5f44394583be1
Carlink <carliianoide@hotmail.com>
2015-05-15 18:46:58 -0500

    Version_1
    Aplicación MEAN
    Modulo Crud: Clientes

---
+++ app/models/cliente.server.model.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Cliente Schema
+ */
+var ClienteSchema = new Schema({
+	name: {
+		type: String,
+		default: '',
+		required: 'Please fill Cliente name',
+		trim: true
+	},
+	boleta: {
+		type: String,
+		default: '',
+		required: 'Please fill Cliente name',
+		trim: true
+	},
+	carrera: {
+		type: String,
+		default: '',
+		required: 'Please fill Cliente name',
+		trim: true
+	},
+	promedio: {
+		type: Number,
+		default: '',
+		required: 'Please fill Cliente name',
+		trim: true
+	},
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	user: {
+		type: Schema.ObjectId,
+		ref: 'User'
+	}
+});
+
+mongoose.model('Cliente', ClienteSchema);
